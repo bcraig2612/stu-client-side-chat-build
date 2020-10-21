cc190a7d592baf2e7c68b5fd079695145bedbd38
Cody Uptain <codyuptain@Codys-Mac-mini.local>
2020-10-21 16:07:06 -0700

    hf3

--- src/components/ConversationMain.js
+++ src/components/ConversationMain.js
@@ -36,8 +36,8 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
     backgroundColor: "#fff",
     [theme.breakpoints.down('sm')]: {
+      height: "100vh",
       maxHeight: "100vh",
-      backgroundColor: "blue"
     },
   },
   header: {
