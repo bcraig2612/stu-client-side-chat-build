9763203fee8302302f656ac1bf4780045836ad95
Cody Uptain <codyuptain@Codys-Mac-mini.local>
2020-10-22 09:41:32 -0700

    hf

--- src/customHooks.js
+++ src/customHooks.js
@@ -8,7 +8,7 @@ export function useQuery() {
 }
 
 export function getJWT() {
-  localStorage.setItem('stu_jwt', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvc290ZWxsdXMuY29tIiwiYXVkIjoiaHR0cHM6XC9cL3NvdGVsbHVzLmNvbSIsImNsaWVudGlkIjoxMjEwLCJpc19jbGllbnRfY29udGFjdCI6MCwiY2hhbm5lbF9uYW1lIjoiY2hhbm5lbF9kZW1vIiwiY2xpZW50X2NvbnRhY3RfaWQiOmZhbHNlLCJleHAiOjE2MDMzODIwNzl9.JoyPgtV-IjDNCMss6aDOVz6MyJfIKHoYwgTJCv9ttZM');
+  localStorage.setItem('stu_jwt', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvc290ZWxsdXMuY29tIiwiYXVkIjoiaHR0cHM6XC9cL3NvdGVsbHVzLmNvbSIsImNsaWVudGlkIjoxMjEwLCJpc19jbGllbnRfY29udGFjdCI6MCwiY2hhbm5lbF9uYW1lIjoiY2hhbm5lbF9kZW1vIiwiY2xpZW50X2NvbnRhY3RfaWQiOmZhbHNlLCJleHAiOjE2MDMzODg0NjJ9.YW3t4d7DwD8v976su_akzVDodNinrQ0pQKIyz8gccOQ');
   const token = localStorage.getItem('stu_jwt');
   if (token) {
     // return for use in Authorization header
